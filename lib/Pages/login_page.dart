7d599aad2a183adde3124d42f1d0688a8c080263
rajendra <rajendra63790@gmail.com>
2022-07-23 18:56:26 +0530

    first Commit

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,68 +1,109 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login_page.png",
-            fit: BoxFit.cover,
-          ),
-          // ignore: prefer_const_constructors
-          SizedBox(
-            height: 20.0,
-          ),
-          const Text(
-            "Welcome",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_page.png",
+              fit: BoxFit.cover,
+              height: 300,
             ),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Enter UserName",
-                    labelText: "UserName",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "PassWord",
-                  ),
-                ),
-              ],
+            // ignore: prefer_const_constructors
+            SizedBox(
+              height: 20,
             ),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
+            Text(
+              "Welcome $name",
+              style: const TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
 
-          ElevatedButton(
-            onPressed: () {
-              log("Hi Random User");
-            },
-            style: TextButton.styleFrom(),
-            child: const Text("login"),
-          ),
-        ],
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter UserName",
+                      labelText: "UserName",
+                    ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "PassWord",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+
+            InkWell(
+              onTap: () async {
+                setState(() {
+                  changeButton = true;
+                });
+                await Future.delayed(const Duration(seconds: 1));
+                // ignore: use_build_context_synchronously
+                Navigator.pushNamed(context, MyRoutes.homeRoute);
+              },
+              child: AnimatedContainer(
+                duration: const Duration(seconds: 1),
+                width: changeButton ? 50 : 150,
+                height: 50,
+                alignment: Alignment.center,
+                // ignore: sort_child_properties_last
+                child: changeButton
+                    ? const Icon(
+                        Icons.done,
+                        color: Colors.white,
+                      )
+                    : const Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(changeButton ? 50 : 8),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Pages/home_page.dart';
 import 'package:flutter_application_1/Pages/login_page.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 // ignore: depend_on_referenced_packages
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,12 +15,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: GoogleFonts.lato().fontFamily,
       ),
       routes: {
         "/": (context) => const LoginPage(),
-        "/home": (context) => const HomePage(),
+        MyRoutes.homeRoute: (context) => const HomePage(),
+        MyRoutes.loginRoute: (context) => const LoginPage(),
       },
     );
   }
